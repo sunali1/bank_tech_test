7934659832ab3fba9a7eed2ae348f6e0478a51c5
sunaliseth <sunaliseth01@gmail.com>
2017-12-11 22:05:41 +0000

    Transaction initializes with a default balance of 0

--- spec/transaction_spec.rb
+++ spec/transaction_spec.rb
@@ -7,6 +7,10 @@ describe Transaction do
     let(:balance) {double :balance}
     subject(:transaction) { described_class.new(credit: credit, debit: debit, balance: balance) }
 
+  it "should respond to initialize with default balance of 0" do
+    expect(Transaction.new.balance).to eq(0)
+  end
+
   it "records the date of transaction" do
     expect(transaction.date).to eq("11-12-2017")
   end
