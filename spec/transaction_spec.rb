fcbd1f75f4fc8488373d3b9bea248e3c38e7b61c
sunaliseth <sunaliseth01@gmail.com>
2017-12-11 17:34:34 +0000

    Transaction can record credit, debit made

--- spec/transaction_spec.rb
+++ spec/transaction_spec.rb
@@ -1,24 +1,25 @@
 require 'transaction'
 
 describe Transaction do
-  subject(:transaction) { described_class.new }
-  let(:credit) { 100 }
-  let(:debit)  { 50 }
-  let(:balance) { 50 }
 
-  it "records the date of credit" do
+    let(:credit) {double :credit}
+    let(:debit) {double :debit}
+    let(:balance) {double :balance}
+    subject(:transaction) { described_class.new(credit: credit, debit: debit, balance: balance) }
+
+  it "records the date of transaction" do
     expect(transaction.date).to eq("11-12-2017")
   end
 
   it "records the credit made" do
-    expect(transaction.credit).to eq(100)
+    expect(transaction.credit).to eq(credit)
   end
 
   it "records the debit made" do
-    expect(transaction.debit).to eq(50)
+    expect(transaction.debit).to eq(debit)
   end
 
   it "records the new balance" do
-    expect(transaction.new_balance).to eq(50)
+    expect(transaction.balance).to eq(balance)
   end
 end
